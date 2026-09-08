3841f05e04b6a1bb3d233c79cac4a12b7e4603dc
Alex Ogorek <alex@alexogorek.com>
2026-09-07 21:09:33 -0400

    initial project commit

---
+++ Flights/Flights/FlightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightsApp.swift
+//  Flights
+//
+//  Created by Alex Ogorek on 9/7/26.
+//
+
+import SwiftUI
+
+@main
+struct FlightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
